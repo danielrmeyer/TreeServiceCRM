f2eabfa04499291a8324687f3aa82d0818bfc0fb
Daniel Meyer <meye1677@gmail.com>
2019-11-22 13:23:16 -0800

    Installed rest framework

--- backend/crm/crm/settings.py
+++ backend/crm/crm/settings.py
@@ -37,10 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #3rd Party
+    'rest-framework',
 
     # Local
     'customers.apps.CustomersConfig',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
