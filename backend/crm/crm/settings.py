2e9dc5c74817107f1ff9e9789c82fab29508b9b0
Daniel Meyer <meye1677@gmail.com>
2019-11-22 14:47:36 -0800

    added corsheaders and fixed a mis-named primary key in urls

--- backend/crm/crm/settings.py
+++ backend/crm/crm/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #3rd Party
     'rest_framework',
+    'corsheaders',
 
     # Local
     'customers.apps.CustomersConfig',
@@ -51,6 +52,8 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,3 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+]
--- backend/crm/customers/urls.py
+++ backend/crm/customers/urls.py
@@ -5,10 +5,10 @@ from .views import ListProperty, DetailProperty
 from .views import ListJob, DetailJob
 
 urlpatterns = [
-    path('<int:customer_id>/customer/', DetailCustomer.as_view()),
+    path('<int:pk>/customer', DetailCustomer.as_view()),
     path('customer/', ListCustomer.as_view()),
-    path('<int:property_id>/property/', DetailProperty.as_view()),
+    path('<int:pk>/property/', DetailProperty.as_view()),
     path('property/', ListProperty.as_view()),
-    path('<int:job_id>/job/', DetailJob.as_view()),
+    path('<int:pk>/job/', DetailJob.as_view()),
     path('job/', ListJob.as_view()),
 ]
