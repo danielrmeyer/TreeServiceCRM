0310f8b8000c66b6b440876f98d9ba7ccc024d35
Daniel Meyer <meye1677@gmail.com>
2019-11-27 13:10:40 -0800

    Added list_display fields and callables so customer name is visable in property and job admin view.

--- customers/admin.py
+++ customers/admin.py
@@ -13,13 +13,35 @@ class CustomerAdmin(admin.ModelAdmin):
         "notes"
     )
 
+    list_display = (
+        "name",
+        "address",
+        "phone1",
+        "phone2",
+        "email",
+        "notes"
+    )
+
 
 class PropertyAdmin(admin.ModelAdmin):
     search_fields = (
         "address",
         "rental",
-        "notes"
-        )
+        "notes",
+    )
+
+    list_display = (
+        "address",
+        "rental",
+        "notes",
+        "get_customer_name"
+    )
+
+    def get_customer_name(self, obj):
+        return obj.customer.name
+
+    get_customer_name.admin_order_field = "customer"
+    get_customer_name.short_description = "Customer Name"
 
 
 class JobAdmin(admin.ModelAdmin):
@@ -27,8 +49,33 @@ class JobAdmin(admin.ModelAdmin):
         "description",
         "order_date",
         "start_date",
-        "notes"
-        )
+        "notes",
+    )
+
+    date_hierarchy = 'start_date'
+
+    list_display = (
+        "description",
+        "order_date",
+        "start_date",
+        "completion_date_time",
+        "cost",
+        "paid",
+        "get_customer_name",
+        "get_property_address",
+    )
+
+    def get_customer_name(self, obj):
+        return obj.customer.name
+
+    def get_property_address(self, obj):
+        return obj.property.address
+
+    get_customer_name.admin_order_field = "customer"
+    get_customer_name.short_description = "Customer Namer"
+
+    get_property_address.admin_order_field = "address"
+    get_property_address.short_description = "Property Address"
 
 
 admin.site.register(Customer, CustomerAdmin)
